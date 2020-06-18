32eaf46e4e26cb46b8e0c46450e27d3a3aa11faf
Noah Melngailis <noah.melngailis@gmail.com>
2020-06-18 09:27:34 -0500

    commit

--- wrangle.py
+++ wrangle.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def acquire_fha_data():
-    """take fha data downloaded into csv and turn it into pandas df"""
+    """takes fha data from hud website (url, sheet_name) turns it into pandas df"""
     
     url = 'https://www.hud.gov/sites/dfiles/Housing/documents/Initi_Endores_Firm%20Comm_DB_FY06_FY20_Q2.xlsx'
     sheet_name = "Firm Cmtmts, Iss'd and Reiss'd"
@@ -15,7 +15,7 @@ def snake_case_column_names(df):
     new_column_names = []
     
     for col in df.columns:
-        new_column_names.append(col.lower().strip(',').replace(" ", "_"))
+        new_column_names.append(col.lower().replace(',', "").replace(" ", "_"))
     
     df.columns = new_column_names
     return df
