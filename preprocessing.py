e83199ea19fedea278e85845b2aafc5b222094ca
Alec Hartman <aleclhartman08@gmail.com>
2020-06-22 10:22:15 -0500

    growth rates calculated; EI solved; preprocessing updated

--- preprocessing.py
+++ preprocessing.py
@@ -25,7 +25,10 @@ def get_model_df():
     # calling wrangle_hud
     df = wr.wrangle_hud()
 
-    # grouping by project_city, project_state, and fiscal_year_of_firm_commitment
+    # creating New Construction DataFrame
+    nc_df = df[df.activity_description == "New Construction"]
+
+    # grouping all data by project_city, project_state, and fiscal_year_of_firm_commitment
     # aggregating final_mortgage_amount
     # sorting by sum and count in descending order
     model_df = df.groupby(["project_city", "project_state", "fiscal_year_of_firm_commitment_activity"])[
@@ -41,12 +44,41 @@ def get_model_df():
             "project_state": "state",
             "fiscal_year_of_firm_commitment_activity": "year",
             "count": "quantity_of_mortgages",
-            "sum": "total_mortgage_amount",
-            "mean": "average_mortgage_amount",
+            "sum": "total_mortgage_volume",
+            "mean": "average_mortgage_volume",
             "median": "median_mortgage_amount",
         },
         inplace=True,
     )
+
+    # grouping nc_df in the same way as the model_df
+    nc_model_df = (
+        nc_df.groupby(
+            ["project_city", "project_state", "fiscal_year_of_firm_commitment_activity"]
+        )["final_mortgage_amount"]
+        .agg(["count", "sum", "mean", "median"])
+        .reset_index()
+    )
+
+    # rename the columns in the nc_model_df
+    nc_model_df.rename(
+        columns={
+            "project_city": "city",
+            "project_state": "state",
+            "fiscal_year_of_firm_commitment_activity": "year",
+            "count": "quantity_of_mortgages",
+            "sum": "total_mortgage_volume",
+            "mean": "average_mortgage_volume",
+            "median": "median_mortgage_amount",
+        },
+        inplace=True,
+    )
+
+    # merging model_df and nc_model_df on city, state, and year and suffixing the features with the same names appropriately
+    combined_df = pd.merge(model_df, nc_model_df, on=["city", "state", "year"], how="left", suffixes=("_pop", "_nc"))
+
+    # reassigning the model_df variable to the combined DataFrame created in the cell above
+    model_df = combined_df
 
     # create label feature
     model_df["label"] = np.where(
@@ -64,28 +96,62 @@ def get_model_df():
 # Feature Engineering #
 # ------------------- #
 
-def calculate_mortgage_var(df):
-    df["mean_var"] = df.sort_values(["city", "state", "year"])["average_mortgage_amount"].pct_change() * 100
+def calculate_city_state_vol_delta(df):
+    """
+    This function creates the specific market growth (city + state observation) rate by doing the following:
+    1. Creates the city_state_growth_pop feature out of the total_mortgage_volume_pop
+    2. Creates the city_state_growth_nc feature out of the total_mortgage_volume_nc
+    3. Returns the df with the new features
+    """
+
+    # create city_state_vol_delta_pop
+    df["city_state_vol_delta_pop"] = df.sort_values(["year"]).groupby(["city", "state"])[["total_mortgage_volume_pop"]].pct_change()
+
+    # create city_state_vol_delta_nc
+    df["city_state_vol_delta_nc"] = df.sort_values(["year"]).groupby(["city", "state"])[["total_mortgage_volume_nc"]].pct_change()
+
     return df
 
-def calculate_mortgage_cnt_var(df):
-    df["quantity_var"] = df.sort_values(["city", "state", "year"])["quantity_of_mortgages"].pct_change() * 100
+def calculate_city_state_qty_delta(df):
+    """
+    This function does the following:
+    1. Creates the quantity_var_pop feature out of the quantity_of_mortgages_pop
+    2. Creates the quantity_var_nc feature out of the quantity_of_mortgages_nc
+    3. Returns the df with the new features
+    """
+
+    # create city_state_qty_delta_pop
+    df["city_state_qty_delta_pop"] = df.sort_values(["year"]).groupby(["city", "state"])[["quantity_of_mortgages_pop"]].pct_change()
+
+    # create city_state_qty_delta_nc
+    df["city_state_qty_delta_nc"] = df.sort_values(["year"]).groupby(["city", "state"])[["quantity_of_mortgages_nc"]].pct_change()
+
     return df
 
 def calculate_evolution_index(df):
+    """
+    This function calculates the evolution index using the market_volume_delta feature created within using the market_volume
+    """
+
     # EI = (1 + Company Growth %) / (1 + Market Growth %) X 100
     df = df.sort_values(["city", "state", "year"])
-    df["market_growth"] = df.groupby("year").mean_var.transform("mean")
 
-    df["ei"] = (1+ df.mean_var) / (1+ df.market_growth) * 100
+    # calc market_volume for the year
+    df["market_volume"] = df.groupby("year").total_mortgage_volume_pop.transform("sum")
+
+    # calculate market growth rate from prior year
+    df["market_volume_delta"] = np.where(df.year > 2006, df["market_volume"].pct_change(), np.nan)
+
+    # calc evolution index
+    df["ei"] = (1 + df.city_state_vol_delta_pop) / (1 + df.market_volume_delta)
 
     return df
 
 #__main prep__
 
 def add_new_features(df):
-    df = calculate_mortgage_var(df)
-    df = calculate_mortgage_cnt_var(df)
+    df = calculate_city_state_vol_delta(df)
+    df = calculate_city_state_qty_delta(df)
     df = calculate_evolution_index(df)
 
     return df
